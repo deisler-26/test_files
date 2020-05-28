41ee80c36f86d8b21d3ef44d07e00887f7ab2d1d
倉内貴成 <kurauchitakanari@kurauchikiseinoiMac.local>
2020-05-28 23:30:05 +0900

    cifar10のkerasモデル

---
+++ cifar10.py
@@ -0,0 +1,87 @@
+#CIFAR-10のデータセットのインポート
+from keras.datasets import cifar10
+(X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+
+
+#CIFAR-10の正規化
+from keras.utils import to_categorical
+
+
+# 特徴量の正規化
+X_train = X_train/255.
+X_test = X_test/255.
+
+# クラスラベルの1-hotベクトル化
+Y_train = to_categorical(Y_train, 10)
+Y_test = to_categorical(Y_test, 10)
+
+# CNNの構築
+import keras
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+import numpy as np
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), padding='same',input_shape=X_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+# コンパイル
+model.compile(loss='categorical_crossentropy',optimizer='SGD',metrics=['accuracy'])
+
+#訓練
+history = model.fit(X_train, Y_train, epochs=100, validation_split=0.1)
+
+# モデルの保存
+model.save('./CIFAR-10.h5')
+
+print(history.history.keys())
+
+#評価 & 評価結果出力
+print(model.evaluate(X_test, Y_test))
+
+#モデルの図示化
+from keras.utils import plot_model
+plot_model(model, to_file='model.png',show_shapes=True)
+
+import matplotlib.pyplot as plt
+
+def plot_history(history):
+    # 精度の履歴をプロット
+    plt.plot(history.history['accuracy'],"o-",label="accuracy")
+    plt.plot(history.history['val_accuracy'],"o-",label="val_accuracy")
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    # 損失の履歴をプロット
+    plt.plot(history.history['loss'],"o-",label="loss",)
+    plt.plot(history.history['val_loss'],"o-",label="val_loss")
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(loc='lower right')
+    plt.show()
+# modelに学習させた時の変化の様子をplot
+plot_history(history)
